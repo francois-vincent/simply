6afd50a099213f950cde4b8f2f4ce8e75f796055
francois-vincent <francois.vincent01@gmail.com>
2016-09-27 15:44:58 +0200

    add pip and pip3 to phusion, update test

--- tests/test_images.py
+++ tests/test_images.py
@@ -8,13 +8,14 @@ from ..platform import factory
 from ..utils import ConfAttrDict
 
 std_python2_path = '/usr/local/lib/python2.7'
+std_python3_path = '/usr/local/lib/python3.'
 
 
 @contextmanager
 def platform_setup(platform):
     platform.setup('uproot' if pytest.config.getoption('--reset') else 'all_containers')
     yield platform
-    if not pytest.config.getoption('--keep'):
+    if not pytest.config.getoption('--keep-running'):
         platform.reset()
 
 
@@ -23,7 +24,7 @@ def test_debian8():
         backend='docker',
         frontend='debian',
         image='debian8',
-        parameters='-i -v {}:/root/simply'.format(ROOTDIR)
+        parameters='-v {}:/root/simply'.format(ROOTDIR)
     )
     with platform_setup(factory(conf)) as platform:
         assert 'Python 2.7' in platform.execute('python -V', stdout_only=False).stderr
@@ -37,7 +38,7 @@ def test_conda2():
         backend='docker',
         frontend='busybox',
         image='conda2',
-        parameters='-i -v {}:/root/simply'.format(ROOTDIR)
+        parameters='-v {}:/root/simply'.format(ROOTDIR)
     )
     with platform_setup(factory(conf)) as platform:
         assert 'Python 2.7' in platform.execute('python -V', stdout_only=False).stderr
@@ -52,7 +53,7 @@ def test_conda3():
         backend='docker',
         frontend='busybox',
         image='conda3',
-        parameters='-i -v {}:/root/simply'.format(ROOTDIR)
+        parameters='-v {}:/root/simply'.format(ROOTDIR)
     )
     with platform_setup(factory(conf)) as platform:
         assert 'Python 3.4' in platform.execute('python -V', stdout_only=False).stderr
@@ -62,15 +63,19 @@ def test_conda3():
         assert python_path in platform.execute('py.test --version', stdout_only=False).stderr
 
 
-def test_phusion_py2():
+def test_phusion():
     conf = ConfAttrDict(
         backend='docker',
         frontend='debian',
         image='phusion',
-        parameters='-i -v {}:/root/simply'.format(ROOTDIR)
+        parameters='-v {}:/root/simply'.format(ROOTDIR)
     )
     with platform_setup(factory(conf)) as platform:
         assert 'Python 2.7' in platform.execute('python -V', stdout_only=False).stderr
-        assert std_python2_path in platform.execute('pip -V')
-        assert platform.execute('pip install pytest')
+        assert std_python2_path in platform.execute('pip2 -V')
+        assert platform.execute('pip2 install pytest')
         assert std_python2_path in platform.execute('py.test --version', stdout_only=False).stderr
+        assert 'Python 3.' in platform.execute('python3 -V')
+        assert std_python3_path in platform.execute('pip3 -V')
+        assert platform.execute('pip3 install pytest')
+        assert std_python3_path in platform.execute('py.test --version', stdout_only=False).stderr
