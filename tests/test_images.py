64553fdbedd8964f5ca1b5da20bd758c08d6e1d2
francois-vincent <francois.vincent01@gmail.com>
2016-09-27 11:08:11 +0200

    fix some Dockerfile, refactor test_images

--- tests/test_images.py
+++ tests/test_images.py
@@ -1,22 +1,35 @@
 # encoding: utf-8
 
+from contextlib import contextmanager
+import pytest
+
 from .. import ROOTDIR
 from ..platform import factory
 from ..utils import ConfAttrDict
 
+std_python2_path = '/usr/local/lib/python2.7'
 
-def test_debian8_py2():
+
+@contextmanager
+def platform_setup(platform):
+    platform.setup('uproot' if pytest.config.getoption('--reset') else 'all_containers')
+    yield platform
+    if not pytest.config.getoption('--keep'):
+        platform.reset('rm_container')
+
+
+def test_debian8():
     conf = ConfAttrDict(
         backend='docker',
         frontend='debian',
-        image='debian8_py2',
+        image='debian8',
         parameters='-i -v {}:/root/simply'.format(ROOTDIR)
     )
-    platform = factory(conf)
-    assert platform.setup('all_containers')
-    assert 'Python 2.7' in platform.execute('python -V', stdout_only=False).stderr
-    assert platform.execute('pip install pytest')
-    assert 'version' in platform.execute('py.test --version', stdout_only=False).stderr
+    with platform_setup(factory(conf)) as platform:
+        assert 'Python 2.7' in platform.execute('python -V', stdout_only=False).stderr
+        assert std_python2_path in platform.execute('pip -V')
+        assert platform.execute('pip install pytest')
+        assert std_python2_path in platform.execute('py.test --version', stdout_only=False).stderr
 
 
 def test_conda2():
@@ -26,11 +39,12 @@ def test_conda2():
         image='conda2',
         parameters='-i -v {}:/root/simply'.format(ROOTDIR)
     )
-    platform = factory(conf)
-    assert platform.setup('all_containers')
-    assert 'Python 2.7' in platform.execute('python -V', stdout_only=False).stderr
-    assert platform.execute('pip install pytest')
-    assert 'version' in platform.execute('py.test --version', stdout_only=False).stderr
+    with platform_setup(factory(conf)) as platform:
+        assert 'Python 2.7' in platform.execute('python -V', stdout_only=False).stderr
+        python_path = '/root/miniconda/lib/python2.7'
+        assert python_path in platform.execute('pip -V')
+        assert platform.execute('pip install pytest')
+        assert python_path in platform.execute('py.test --version', stdout_only=False).stderr
 
 
 def test_conda3():
@@ -40,22 +54,23 @@ def test_conda3():
         image='conda3',
         parameters='-i -v {}:/root/simply'.format(ROOTDIR)
     )
-    platform = factory(conf)
-    assert platform.setup('all_containers')
-    assert 'Python 3.' in platform.execute('python -V', stdout_only=False).stderr
-    assert platform.execute('pip install pytest')
-    assert 'version' in platform.execute('py.test --version', stdout_only=False).stderr
+    with platform_setup(factory(conf)) as platform:
+        assert 'Python 3.4' in platform.execute('python -V', stdout_only=False).stderr
+        python_path = '/root/miniconda3/lib/python3.4'
+        assert python_path in platform.execute('pip -V')
+        assert platform.execute('pip install pytest')
+        assert python_path in platform.execute('py.test --version', stdout_only=False).stderr
 
 
-def test_phusion():
+def test_phusion_py2():
     conf = ConfAttrDict(
         backend='docker',
         frontend='debian',
         image='phusion',
         parameters='-i -v {}:/root/simply'.format(ROOTDIR)
     )
-    platform = factory(conf)
-    assert platform.setup('all_containers')
-    assert 'Python 2.7' in platform.execute('python -V', stdout_only=False).stderr
-    assert platform.execute('pip install pytest')
-    assert 'version' in platform.execute('py.test --version', stdout_only=False).stderr
+    with platform_setup(factory(conf)) as platform:
+        assert 'Python 2.7' in platform.execute('python -V', stdout_only=False).stderr
+        assert std_python2_path in platform.execute('pip -V')
+        assert platform.execute('pip install pytest')
+        assert std_python2_path in platform.execute('py.test --version', stdout_only=False).stderr
